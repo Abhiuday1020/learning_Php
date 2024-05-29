0d1f16590256342393255af1f26fe5e7e3b86b08
Abhiuday <abhiuday1020@gmail.com>
2024-05-29 22:35:01 +0530

    29-05-24

--- 1.php
+++ 1.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello</title>
+    <title>php learning</title>
 </head>
 <body>
-    <h1>good Morning</h1>
+    <h1>good Morning <?php echo "everyone" ?> </h1>
     <?php 
 
     $name = "Abhiuday";
-    echo "Hello $name";
-    
+    echo "Hello $name <br>";
+    echo "This is abhiuday!";
     ?>
 </body>
 </html>
--- array.php
+++ array.php
@@ -1,16 +1,28 @@
 <?php
 
-$guitar = ['Vela','Explorer','Strat'];
+// traditinal array
+// $guitar1 = ['Vela','Explorer','Strat'];
 
-echo $guitar[0] ."<br>";
+// echo $guitar1[0] ."<br>";
 
-if(isset($guitar[4]))
-{
-    echo $guitar[4];
+// if(isset($guitar1[4]))
+// {
+//     echo $guitar1[4];
+// }
+// else
+// {
+//     echo "guitar does not exist<br>";
+// }
+
+$guitars2 = [
+    'prs' => 'Vela',
+    'gibson' => 'Explorer',
+    'fender'=> 'Strat',
+];
+
+if(isset($guitars2['prs'])){
+    echo $guitars2['prs'];
 }
-else
-{
+else{
     echo "guitar does not exist<br>";
 }
-
-?> 
